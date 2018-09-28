782644046bde485a283cd6252303836075f50657
Daniel Ma <drailskid@yahoo.com>
2018-09-27 22:32:17 -0700

    Initial Commit

---
+++ Spotivoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotivoice
+//
+//  Created by Daniel Ma on 9/27/18.
+//  Copyright © 2018 DMa Unsure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
